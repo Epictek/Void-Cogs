20c811cfaa1deaf63adde4fefb8379ab9368ccbd
Kieran Coldron <kieran@coldron.com>
2018-09-12 14:59:38 +0000

    fix some bugs with some messy code

--- xbps/xbps.py
+++ xbps/xbps.py
@@ -25,21 +25,36 @@ class xbps:
         links = []
         length = 0
         embeds = []
-        for package in r.json()["data"]:
-            packageString =("[" + package["name"] + " " + str(package["version"]) + "_" + str(package["revision"]) +"]" +
-                            "(https://github.com/void-linux/void-packages/tree/master/srcpkgs/"
-                            + package["name"] + ") - " + package["desc"])
-            if len(packageString) + length < 2000:
-                links.append(packageString)
-                length += len(packageString)
-            else: 
-                em = discord.Embed(title='Void Repo Search: ' + searchterm , url="https://github.com/void-linux/void-packages/search?q%5B%5D=filename%3Atemplate+path%3A%2Fsrcpkgs&q%5B%5D=" + urlterm + "&s=indexed)" ,description='\n'.join(links), colour=0x478061)
-                em.set_footer(text="Search results from https://xbps.spiff.io", icon_url="https://voidlinux.org/assets/img/void_bg.png")
-                embeds.append(em)
-                links = []
-                length = 0
-        print(embeds)
-        await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS)
+
+
+        if len(r.json()["data"]) == 0:
+            em = discord.Embed(title='Void Repo Search: ' + searchterm , url="https://github.com/void-linux/void-packages/search?q%5B%5D=filename%3Atemplate+path%3A%2Fsrcpkgs&q%5B%5D=" + urlterm + "&s=indexed)", description='No packages found', colour=0x478061)
+            em.set_footer(text="Search results from https://xbps.spiff.io", icon_url="https://voidlinux.org/assets/img/void_bg.png")
+            await ctx.send(embed=em)
+
+        else:
+            for package in r.json()["data"]:
+                packageString =("[" + package["name"] + " " + str(package["version"]) + "_" + str(package["revision"]) +"]" +
+                                "(https://github.com/void-linux/void-packages/tree/master/srcpkgs/"
+                                + package["name"] + ") - " + package["desc"])
+
+                if len(packageString) + length < 2000:
+                    links.append(packageString)
+                    length += len(packageString)
+                else:
+                    em = discord.Embed(title='Void Repo Search: ' + searchterm , url="https://github.com/void-linux/void-packages/search?q%5B%5D=filename%3Atemplate+path%3A%2Fsrcpkgs&q%5B%5D=" + urlterm + "&s=indexed)" ,description='\n'.join(links), colour=0x478061)
+                    em.set_footer(text="Search results from https://xbps.spiff.io", icon_url="https://voidlinux.org/assets/img/void_bg.png")
+                    embeds.append(em)
+                    links = []
+                    links.append(packageString)
+                    length = len(packageString)
+            em = discord.Embed(title='Void Repo Search: ' + searchterm , url="https://github.com/void-linux/void-packages/search?q%5B%5D=filename%3Atemplate+path%3A%2Fsrcpkgs&q%5B%5D=" + urlterm + "&s=indexed)" ,description='\n'.join(links), colour=0x478061)
+            em.set_footer(text="Search results from https://xbps.spiff.io", icon_url="https://voidlinux.org/assets/img/void_bg.png")
+            embeds.append(em)
+            if len(embeds) > 1:
+                await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS)
+            else:
+                await ctx.send(embed=em)
 
 def setup(bot):
     bot.add_cog(xbps(bot))
